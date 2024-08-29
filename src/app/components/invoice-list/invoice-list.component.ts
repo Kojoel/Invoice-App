822989724a31d756b31c19480ac30e050284c542
Kojoel <nanaabruquah1s@gmail.com>
2024-08-29 01:41:50 -0700

    Displayed data on data list component

--- src/app/components/invoice-list/invoice-list.component.ts
+++ src/app/components/invoice-list/invoice-list.component.ts
@@ -4,33 +4,34 @@ import { Invoice } from '../../model/invoice.model';
 import { Store } from '@ngrx/store';
 import { selectAllInvoices } from '../../store/invoice.selectors';
 import { loadInvoices } from '../../store/invoice.actions';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule, NgIf } from '@angular/common';
+import { DataService } from '../../services/data.service';
 
 
 @Component({
   selector: 'app-invoice-list',
   standalone: true,
-  imports: [CommonModule], // Use valid standalone components, directives, pipes, or NgModules
+  imports: [CommonModule, NgIf, AsyncPipe], // Use valid standalone components, directives, pipes, or NgModules
   templateUrl: './invoice-list.component.html',
   styleUrls: ['./invoice-list.component.scss']
 })
 export class InvoiceListComponent implements OnInit {
   invoices$: Observable<Invoice[]>;
 
-  invoiceItems : Invoice[] = [];
-
-  constructor(private store: Store) {
-    this.invoices$ = this.store.select(selectAllInvoices);
+  constructor(private store: Store, private dataService: DataService) {
+    // this.invoices$ = this.store.select(selectAllInvoices);
+    this.invoices$ = this.dataService.getInvoices();
   }
 
   ngOnInit(): void {
     this.store.dispatch(loadInvoices());
 
-    this.invoices$.subscribe(item => this.invoiceItems = item);
-    console.log(this.invoiceItems);
-  }
+    // this.invoices$.subscribe(invoices => {
+    //   console.log('Array of Invoices:', invoices);
+    // });
 
-  // trackInvoice(index: number, invoice: Invoice): string {
-  //   return invoice.id;
-  // }
+    this.dataService.getInvoices().subscribe(invoices => {
+      console.log('Array of Invoices:', invoices);
+    });
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Invoice } from '../model/invoice.model';
+import { Invoice } from '../model/invoice.model' ;
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  private dataUrl = './assets/data/invoices.json';
+  private dataUrl = '../../assets/data/data.json';
+
+  invoiceData: Invoice[] = [];
 
   constructor(private http: HttpClient) { }
 
   getInvoices(): Observable<Invoice[]> {
     return this.http.get<Invoice[]>(this.dataUrl);
   }
+
 }
