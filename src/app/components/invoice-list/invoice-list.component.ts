e75586ddca156065d88066f320b91f03312d8721
Kojoel <nanaabruquah1s@gmail.com>
2024-08-29 03:21:40 -0700

    Choose filter status UI complete

--- src/app/components/invoice-list/invoice-list.component.ts
+++ src/app/components/invoice-list/invoice-list.component.ts
@@ -11,27 +11,37 @@ import { DataService } from '../../services/data.service';
 @Component({
   selector: 'app-invoice-list',
   standalone: true,
-  imports: [CommonModule, NgIf, AsyncPipe], // Use valid standalone components, directives, pipes, or NgModules
+  imports: [CommonModule, NgIf, AsyncPipe],
   templateUrl: './invoice-list.component.html',
   styleUrls: ['./invoice-list.component.scss']
 })
 export class InvoiceListComponent implements OnInit {
   invoices$: Observable<Invoice[]>;
 
+  //Variables for toggling
+  filterClicked: boolean = false;
+
+
   constructor(private store: Store, private dataService: DataService) {
-    // this.invoices$ = this.store.select(selectAllInvoices);
-    this.invoices$ = this.dataService.getInvoices();
+    this.invoices$ = this.store.select(selectAllInvoices); // Gets data from store
+    // this.invoices$ = this.dataService.getInvoices();  // Gets data from service
   }
 
   ngOnInit(): void {
     this.store.dispatch(loadInvoices());
 
-    // this.invoices$.subscribe(invoices => {
-    //   console.log('Array of Invoices:', invoices);
-    // });
-
-    this.dataService.getInvoices().subscribe(invoices => {
+    // Subscribe to obeservable from store...
+    this.invoices$.subscribe(invoices => {
       console.log('Array of Invoices:', invoices);
     });
+
+    // SUbscribe to observable from data service...
+    // this.dataService.getInvoices().subscribe(invoices => {
+    //   console.log('Array of Invoices:', invoices);
+    // });
+  }
+
+  togglefilter() {
+    this.filterClicked = !this.filterClicked;
   }
 }
